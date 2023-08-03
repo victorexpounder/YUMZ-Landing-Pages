04dbf7eb5c7c6c499ab0323545a103f239363b49
Eze Victor Expounder <109489104+victorexpounder@users.noreply.github.com>
2023-08-03 02:42:56 +0100

    full homePage
    full homePage competed with Responsiveness and interactivity

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { HomePage } from "./Pages/HomePage/HomePage";
+
+
+function App(){
+    return(
+         <div className="container">
+            <HomePage></HomePage>
+         </div>
+
+         
+    )
+}
+export default App;
